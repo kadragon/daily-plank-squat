eee6d2c2fe069490cba35b185d7017a87d9512d6
Kang, Donguk <kangdongouk@gmail.com>
2026-02-19 21:44:48 +0900

    address pwa review feedback

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app'
@@ -7,12 +7,20 @@ import { registerPwaUpdater } from './pwa-registration'
 const root = document.getElementById('root')
 if (!root) throw new Error('Root element not found')
 
-if ('serviceWorker' in navigator) {
-  registerPwaUpdater()
+function AppWithPwaUpdater() {
+  useEffect(() => {
+    if (!('serviceWorker' in navigator)) {
+      return
+    }
+
+    return registerPwaUpdater()
+  }, [])
+
+  return <App />
 }
 
 createRoot(root).render(
   <StrictMode>
-    <App />
+    <AppWithPwaUpdater />
   </StrictMode>,
 )
